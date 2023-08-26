497f7d64903a60908da4b7f0e81f84784b14afcd
LucasAlAlam <lucasaars@dcc.ufrj.br>
2023-08-26 01:40:57 +0000

    init: frist commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('./src/config/dotenv')();
+//require('./src/config/sequelize');
+
+const app = express();
+const port = process.env.PORT;
+//const cors = require('cors');
+//const routes = require('./src/routes/routes');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use(routes);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`undefined app listening at http://localhost:${port}`);
+});
