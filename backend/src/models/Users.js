b241a650f02c29f8fd4ad837842e33523001ec1d
LucasAlAlam <lucasaars@dcc.ufrj.br>
2023-08-27 07:42:34 +0000

    remove: auth and salt

--- backend/src/models/Users.js
+++ backend/src/models/Users.js
@@ -2,12 +2,6 @@ const DataTypes = require('sequelize');
 const sequelize = require('../config/sequelize');
 
 const Users = sequelize.define('Users', {
-    salt:{
-        type: DataTypes.STRING
-    },
-    hash:{
-        type: DataTypes.STRING
-    },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -26,6 +20,7 @@ Users.associate = (models) => {
     Users.hasMany(models.Address);
     Users.hasMany(models.Contact);
     Users.hasMany(models.PaymentMethod);
+    Users.hasOne(models.Auth);
     Users.belongsToMany(models.Users, {
         through: 'Avaliations',
         as: 'Avaliator',
