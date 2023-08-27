cb5d9e282e435dbc8b5ab858d702d08be3ef9806
LucasAlAlam <lucasaars@dcc.ufrj.br>
2023-08-27 07:42:08 +0000

    bugfix: ajust for context of auth decoupled

--- backend/src/controllers/UsersContoller.js
+++ backend/src/controllers/UsersContoller.js
@@ -1,9 +1,18 @@
+const authModel = require('../models/Auth');
 const avaliationModel = require('../models/Avaliation');
 const usersModel = require('../models/Users');
 
 const create = async (req, res) => {
     try {
-        const user = await usersModel.create(req.body);
+        const user = await usersModel.create({
+            email: req.body.email,
+            name: req.body.name
+        });
+        await authModel.create({
+            hash: req.body.hash,
+            salt: req.body.salt,
+            UserId: user.id,
+        })
         return res.status(201).json({
             message: 'Usuário Cadastrado com sucesso', 
             user: user
@@ -56,6 +65,7 @@ const destroy = async (req, res) => {
     const { id } = req.params;
     try {
         const deleted = await usersModel.destroy({where:{id:id}});
+        await authModel.destroy({where:{UserId:id}});
         if (deleted){
             return res.status(200).json({message: 'Usuário deletado com sucesso'})
         } else throw new Error();
@@ -64,7 +74,7 @@ const destroy = async (req, res) => {
     }
 }
 
-const avaliation = async (req, res) => {
+const avaliate = async (req, res) => {
     const { idAvaliator } = req.params;
     try {
         const avaliation = await avaliationModel.create({
@@ -78,11 +88,23 @@ const avaliation = async (req, res) => {
     }
 }
 
+const avaliations = async (req,res) => {
+    const {id} = req.params;
+    try {
+        const user = await usersModel.findByPk(id);
+        const avaliations = await user.getAvalied();
+        return res.status(200).json(avaliations);
+    } catch (err) {
+        return res.status(500).json({error:err});
+    }
+}
+
 module.exports = {
     create,
     index,
     show,
     update,
     destroy,
-    avaliation
+    avaliate,
+    avaliations
 }
