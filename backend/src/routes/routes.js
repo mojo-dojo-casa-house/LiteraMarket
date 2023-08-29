2baaf0af1aa29af96ac4903b4b6a0b916ee0ef96
LucasAlAlam <lucasaars@dcc.ufrj.br>
2023-08-29 05:39:22 +0000

    feature: routes to cart, favorites and comments

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -39,4 +39,24 @@ router.post('/user/:userId/pixkeys/add', pixKeysController.create);
 router.get('/user/:userId/pixkeys', pixKeysController.index);
 router.delete('/user/:userId/pixkeys/delete', pixKeysController.destroy);
 
+
+//Rotas ainda não testadas
+const commentController = require('../controllers/CommentsController');
+
+router.post('/user/:userId/books/bookId/addComment', commentController.create);
+router.put('/user/:userId/books/bookId/editComment', commentController.update);
+router.delete('/user/:userId/books/bookId/deleteComment', commentController.destroy);
+
+const favoriteController = require('../controllers/FavoritesController')
+
+router.post('/user/:userId/addfavorite', favoriteController.create);
+router.get('/user/:userId/favorites', favoriteController.index);
+router.delete('/user/:userId/deletefavorite', favoriteController.destroy);
+
+const CartController = require('../controllers/CartController')
+
+router.post('/user/:userId/addcart', CartController.create);
+router.get('/user/:userId/cart', CartController.index);
+router.delete('/user/:userId/deletecart', CartController.destroy);
+
 module.exports = router;
